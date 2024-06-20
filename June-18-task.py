30b9a33b29e65cf7642f19bd7d25be29c8d21027
Akileshwaran <67944263+ak2205@users.noreply.github.com>
2024-06-20 23:14:54 +0530

    Add files via upload
    Task of June 18 (String Slicing)

---
+++ June-18-task.py
@@ -0,0 +1,69 @@
+#Program for string slicing Type 2 and Type 1
+#String in focus: "go_kubernetes_laptop" and Type 2(String Slicing)
+print("String Slicing Type 2")
+h = "go_kubernetes_laptop"
+print("String provided is: ", h)
+print(h[3:10:1])
+print(h[3:10:2])
+print(h[3:10:3])
+print(h[5:-5:1])
+print(h[5:-5:2])
+print(h[5:-5:5])
+print(h[:8:4])
+print(h[5::3])
+print(h[::2])
+print(h[12:5:3])
+
+print("-------------------------------------------------------------")
+
+# Type 1(String Slicing)
+
+print("String Slicing Type 1")
+
+print(h[3:8])
+print(h[5:-5])
+print(h[-12:-7])
+print(h[-10:15])
+print(h[:6])
+print(h[:-7])
+print(h[5:])
+print(h[-10:])
+print(h[:])
+print(h[4:100])
+print(h[12:5])
+
+print("-------------------------------------------------------------")
+
+#String in focus: "stop_share_screen" and Type 2(String Slicing)
+
+y = "stop_share_screen"
+print("String Slicing Type 2")
+print("String in focus is: ", y)
+print(y[3:10:1])
+print(y[3:10:2])
+print(y[3:10:3])
+print(y[5:-5:1])
+print(y[5:-5:2])
+print(y[5:-5:5])
+print(y[:8:4])
+print(y[5::3])
+print(y[::2])
+print(y[12:5:3])
+
+print("-------------------------------------------------------------")
+
+# Type 1(String Slicing)
+
+print("String Slicing Type 1")
+
+print(y[3:8])
+print(y[5:-5])
+print(y[-12:-7])
+print(y[-10:15])
+print(y[:6])
+print(y[:-7])
+print(y[5:])
+print(y[-10:])
+print(y[:])
+print(y[4:100])
+print(y[12:5])
